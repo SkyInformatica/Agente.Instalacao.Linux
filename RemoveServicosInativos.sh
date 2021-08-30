1ef895ba4a1de4a8f50209f9c71b094238f8b7f5
Alex Junior Vieira <alex.vieira@skyinformatica.com.br>
2021-08-30 16:37:23 -0300

    Ajustes no instalador e adicionado limpza de svc

---
+++ RemoveServicosInativos.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+NOMESERVICO=SkyAgenteNovoProjeto
+
+tipoServico=$(ps -p 1 |awk '{print $4}' |sed "1d")
+
+if [ "$tipoServico" = "systemd" ]; then
+
+  for SERVICO in $(ls /etc/systemd/system/ | grep $NOMESERVICO)
+  do
+
+    if [ "$(systemctl is-active $SERVICO)" != "active" ]; then
+      systemctl stop $SERVICO
+      systemctl disable $SERVICO
+      rm /etc/systemd/system/$SERVICO
+    fi
+
+  done
+
+elif [ "$tipoServico" = "init"]; then
+
+  for SERVICO in $(ls /etc/init.d/ | grep $NOMESERVICO)
+  do
+
+    if [ "$(service $SERVICO status)" != "$SERVICO: Rodando." ]; then
+      echo "Vai pra vala caraiiiiiiii: $SERVICO"
+#      update-rc.d -f $SERVICO remove
+#      rm /etc/systemd/system/$SERVICO
+
+    fi
+
+  done
+
+else
+
+  echo "Systema não suportado" 
+
+fi
